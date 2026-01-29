325cd17d99846cbdd13dce2d097629dafec4a079
Valerii Chernysh <argon3300@gmail.com>
2026-01-29 13:21:08 +0200

    maven

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -1,10 +1,19 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 
-
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
-    private UserService userService = new UserService();
+    @Mock
+    private UserDAO userDAO;
+
+    @InjectMocks
+
+    private UserService userService ;
     @Test
     void shouldThrowExceptionWhenUsernameIsEmpty(){
         User usersa = new User("" , "100");
